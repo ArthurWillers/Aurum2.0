93e53016d6d56be3c3250e587e2c90ede4dde19b
willers <arthur2008willers@gmail.com>
2025-08-23 18:37:24 -0300

    feat(routes): add dashboard route within auth middleware

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/login');
+
+Route::middleware('auth')->group(function () {
+  Route::get('/dashboard', function () {dd('dashboard');})->name('dashboard'); // temporário
+});
