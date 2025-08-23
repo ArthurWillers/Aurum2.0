5b0c81e7b7e56ee432e3173f66eb7e23dc0c484b
willers <arthur2008willers@gmail.com>
2025-08-23 18:42:08 -0300

    feat(dashboard): implement dashboard view and logout form (temporary)

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+<h1>dashboard temporária</h1>
+
+{{-- Formulário de Logout --}}
+<form method="POST" action="{{ route('logout') }}">
+    @csrf
+
+    <x-button type="submit" color="light">
+        Sair
+    </x-button>
+</form>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 
 Route::redirect('/', '/login');
 
 Route::middleware('auth')->group(function () {
-  Route::get('/dashboard', function () {dd('dashboard');})->name('dashboard'); // temporário
+  Route::view('/dashboard', 'dashboard')->name('dashboard'); // temporário
+
+  Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout'); //temporário
 });
