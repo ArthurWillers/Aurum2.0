03407d37ea473612a754225261556b9cc9ffd00a
willers <arthur2008willers@gmail.com>
2025-08-24 19:53:54 -0300

    refactor(dashboard): remove temporary dashboard content and logout form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,3 @@
 <x-layouts.app>
-    <h1>dashboard temporária</h1>
-
-    {{-- Formulário de Logout --}}
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
     
-        <x-button type="submit" color="light">
-            Sair
-        </x-button>
-    </form>
 </x-layouts.app>
