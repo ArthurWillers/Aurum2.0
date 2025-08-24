169fe282ab8941caefc69bfe521427ff8bc8549c
willers <arthur2008willers@gmail.com>
2025-08-24 18:27:35 -0300

    feat(logo): add app logo component to the layout

---
+++ resources/views/components/app-logo.blade.php
@@ -0,0 +1,9 @@
+<div class="flex items-center">
+    <div
+        class="flex aspect-square size-8 items-center justify-center rounded-md bg-neutral-800 dark:bg-white">
+        <x-app-logo-icon class="size-5" />
+    </div>
+    <div class="ms-1 grid flex-1 text-start text-lg">
+        <span class="ms-1 truncate leading-tight font-semibold text-neutral-800 dark:text-white">Aurum</span>
+    </div>
+</div>
