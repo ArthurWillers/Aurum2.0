d07671c607d5fb998ecadeb29be7117243b6399f
willers <arthur2008willers@gmail.com>
2025-08-25 14:49:47 -0300

    feat(dashboard): add header  to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,13 @@
 <x-layouts.app>
-    
+  {{-- Cabeçalho --}}
+  <div class="flex justify-between items-center">
+      <h2 class="text-2xl font-bold">Dashboard</h2>
+      <x-button href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+          </svg>
+          Nova Transação
+      </x-button>
+  </div>
 </x-layouts.app>
