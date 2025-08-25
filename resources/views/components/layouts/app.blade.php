2df38b6bdf31f6915c8e2f24d9eeda8a51117606
willers <arthur2008willers@gmail.com>
2025-08-25 14:05:15 -0300

    feat(layout): implement mobile-friendly sidebar toggle functionality

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,13 +7,26 @@
 
 <body class="min-h-screen antialiased bg-neutral-50 dark:bg-neutral-900 text-neutral-800 dark:text-white">
 
-    <div>
-
+    <div x-data="{ open: false }">
         <aside
-            class="fixed top-0 left-0 h-screen w-64 border-e bg-neutral-100 dark:bg-neutral-950 border-neutral-300 dark:border-neutral-800 p-4 flex flex-col gap-4">
-            <a href="{{ route('dashboard') }}">
-                <x-app-logo />
-            </a>
+            class="fixed top-0 left-0 h-screen w-64 border-e bg-neutral-100 dark:bg-neutral-950 border-neutral-300 dark:border-neutral-800 p-4 flex flex-col gap-4 z-40
+                   transition-transform duration-300 ease-in-out
+                   lg:translate-x-0"
+            :class="{ '-translate-x-full': !open }">
+            <div class="flex items-center">
+                <a href="{{ route('dashboard') }}">
+                    <x-app-logo />
+                </a>
+
+                <button @click="open = !open"
+                    class="ms-auto lg:hidden cursor-pointer p-1 rounded-md hover:bg-neutral-200 dark:hover:bg-neutral-700">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d=" M6 18 18 6M6 6l12 12" />
+                    </svg>
+
+                </button>
+            </div>
 
             <nav>
 
@@ -34,10 +47,9 @@
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                 stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                                    d=" M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                             </svg>
                         </div>
-                    </button>
                 </x-slot>
 
                 <x-slot name="content">
@@ -89,8 +101,17 @@
 
         </aside>
 
+        <div x-show="open" @click="open = false" class="fixed inset-0 bg-black/10 z-30 lg:hidden" x-cloak></div>
+
         <header class="flex items-center px-6 w-full min-h-14 lg:hidden">
-            {{-- Botão para abrir a navbar --}}
+            <button @click="open = !open"
+                class="cursor-pointer rounded-lg p-1 hover:bg-neutral-200 dark:hover:bg-neutral-800">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            </button>
 
             <x-dropdown position="bottom-end" class="ms-auto" accent contentClass="w-60">
                 <x-slot name="trigger">
@@ -131,8 +152,8 @@
 
                     <a href="#" @click="open = !open"
                         class="flex w-full items-center gap-2 rounded-md px-2 py-2 text-left text-sm text-neutral-700 hover:bg-neutral-200 dark:text-neutral-200 dark:hover:bg-neutral-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="size-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="size-5">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -143,7 +164,8 @@
 
                     <button type="submit" form="logout" @click="open = !open"
                         class="flex w-full items-center gap-2 rounded-md px-2 py-2 text-left text-sm text-red-500 hover:bg-neutral-200 dark:hover:bg-neutral-800 cursor-pointer">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                            class="size-5">
                             <path fill-rule="evenodd"
                                 d="M3 4.25A2.25 2.25 0 0 1 5.25 2h5.5A2.25 2.25 0 0 1 13 4.25v2a.75.75 0 0 1-1.5 0v-2a.75.75 0 0 0-.75-.75h-5.5a.75.75 0 0 0-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 0 0 .75-.75v-2a.75.75 0 0 1 1.5 0v2A2.25 2.25 0 0 1 10.75 18h-5.5A2.25 2.25 0 0 1 3 15.75V4.25Z"
                                 clip-rule="evenodd" />
@@ -157,11 +179,10 @@
                 </x-slot>
             </x-dropdown>
         </header>
-
-        <main class="ml-64 p-6">
-            {{ $slot }}
-        </main>
     </div>
+    <main class="ml-64 p-6">
+        {{ $slot }}
+    </main>
 
 </body>
 
