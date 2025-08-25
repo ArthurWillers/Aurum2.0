592b6ba311efc65d54b616f6bb5e6cc50575e460
willers <arthur2008willers@gmail.com>
2025-08-25 12:36:51 -0300

    refactor(dropdown): add start and end position for dropdown component

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -4,27 +4,33 @@
     'accent' => false,
 ])
 
+@php
+    $posClasses = [
+        'top' => 'bottom-full mb-2 left-1/2 -translate-x-1/2',
+        'top-start' => 'bottom-full mb-2 left-0',
+        'top-end' => 'bottom-full mb-2 right-0',
+        'bottom' => 'top-full mt-2 left-1/2 -translate-x-1/2',
+        'bottom-start' => 'top-full mt-2 left-0',
+        'bottom-end' => 'top-full mt-2 right-0',
+    ];
+    $dropdownPosition = $posClasses[$position] ?? $posClasses['top'];
+@endphp
+
 <div x-data="{ open: false }" @click.outside="open = false" {{ $attributes->merge(['class' => 'relative']) }}>
-    
+
     {{-- Slot para o gatilho (Botão) --}}
     <div @click="open = !open">
         {{ $trigger }}
     </div>
 
     {{-- Slot para o conteudo --}}
-    <div 
-        x-show="open" 
-        x-transition
-        x-cloak
-        @class([
-            'absolute z-50 rounded-lg border bg-white p-1 shadow-lg dark:bg-neutral-900',
-            'bottom-full mb-2' => $position === 'top', // Abre para cima
-            'top-full mt-2' => $position === 'bottom', // Abre para baixo
-            'border-accent' => $accent,
-            'border-neutral-300 dark:border-neutral-700' => !$accent,
-            $contentClass,
-        ])
-    >
+    <div x-show="open" x-transition x-cloak @class([
+        'absolute z-50 rounded-lg border bg-white p-1 shadow-lg dark:bg-neutral-900',
+        $dropdownPosition,
+        'border-accent' => $accent,
+        'border-neutral-300 dark:border-neutral-700' => !$accent,
+        $contentClass,
+    ])>
         {{ $content }}
     </div>
 
